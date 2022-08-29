b16360528fb46b19627157ec339acffcec4e0a62
Poryadin <vsevporyadin@gmail.com>
2022-08-29 20:26:20 +0500

    new2

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$('.slider-top').owlCarousel({
+    center: true,
+    items:1.5,
+    loop:true,
+    margin:80,
+    responsive:{
+        600:{
+            items:1.5
+        }
+    }
+});
+});
