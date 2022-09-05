1d52a70242449b4e17d5f19a5ab20325b78d2f4f
Alexander <vsevporyadin@gmail.com>
2022-09-05 18:15:16 +0500

    new 4

--- src/js/script.js
+++ src/js/script.js
@@ -25,21 +25,93 @@ $(document).ready(function () {
 				margin:80,
 				items:1.5
 			},
-			1370:{
+			1850:{
 				margin:80,
-				items:1.5
+				items:1.45
 			},
-			1360:{
-				margin:40,
-				items:1.5
+			1750:{
+				margin:80,
+				items:1.4
 			},
-			600:{
-				margin:30,
+			1700:{
+				margin:80,
+				items:1.35
+			},
+			1650:{
+				margin:50,
 				items:1.3
 			},
-			320:{
-				margin:20,
+			1600:{
+				margin:40,
+				items:1.3
+			},
+			1550:{
+				margin:40,
+				items:1.25
+			},
+			1500:{
+				margin:40,
+				items:1.2
+			},
+			1450:{
+				margin:40,
+				items:1.16
+			},
+			1400:{
+				margin:40,
+				items:1.13
+			},
+			1350:{
+				margin:40,
+				items:1.08
+			},
+			1300:{
+				margin:40,
+				items:1.05
+			},
+			1250:{
+				margin:40,
+				items:1.07
+			},
+			1200:{
+				margin:40,
+				items:1.05
+			},
+			1100:{
+				margin:40,
+				items:1.07
+			},
+			1000:{
+				margin:40,
+				items:1.07
+			},
+			900:{
+				margin:40,
+				items:1.07
+			},
+			800:{
+				margin:40,
+				items:1.09
+			},
+			700:{
+				margin:40,
 				items:1.1
+			},
+			600:{
+				margin:40,
+				items:1.12
+			},
+			500:{
+				margin:40,
+				items:1.12
+			},
+			400:{
+				margin:20,
+				items:1.2
+			},
+			320:{
+				margin:10,
+				items:1.2
 			}
     }
 	});
@@ -50,11 +122,23 @@ $(document).ready(function () {
     margin:35,
 		nav: true,
 		navText: ["","<div class='right-btn-slider'><img src='./src/media/icon/right-arrow.svg' alt=''></div>"],
-    // responsive:{
-		// 	600:{
-		// 		margin:30,
-		// 		items:1.3
-		// 	}
-    // }
+    responsive:{
+			950:{
+				margin:35,
+				items:4
+			},
+			800:{
+				margin:35,
+				items:3
+			},
+			600:{
+				margin:35,
+				items:2
+			},
+			320:{
+				margin:35,
+				items:1
+			}
+    }
 	});
 });
